cafb193074e6e1e682c524a2083ffce2f669c1b4
Loubna <loubna.alfidi@gmail.com>
2023-11-11 21:47:02 +0100

    updating console

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''TestBaseModel'''
+
+
+from contextlib import AbstractContextManager
+from typing import Any
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """test module for BaseModel"""
+    def test_id(self):
+        """test id"""
+        base = BaseModel()
+        self.assertIsInstance(base.id, str)
