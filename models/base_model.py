03e31cc36f0ddccc8cc72771f903b863b72ef169
HichamLuffy <imalxvssteve2020@gmail.com>
2023-11-12 00:41:48 +0100

    fix comment

--- models/base_model.py
+++ models/base_model.py
@@ -27,17 +27,15 @@ class BaseModel:
         """
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
-            del kwargs["__class__"]
-            for key, val in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    val = datetime.strptime(val, time_format)
-                    setattr(self, key, val)
-                else:
-                    setattr(self, key, val)
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            self.created_at = datetime.strptime(self.created_at, time_format)
+            self.updated_at = datetime.strptime(self.updated_at, time_format)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
         storage.new(self)
 
     def __str__(self):
