90854498ec8d22bcf57c2ee0229dc6aa2430063e
HichamLuffy <imalxvssteve2020@gmail.com>
2023-11-11 23:53:10 +0100

    fix comment

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""FileStorage"""
+
 
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''FileStorage'''
+"""FileStorage"""
 
 
 from json import dump
@@ -7,21 +7,21 @@ from json import load
 
 
 class FileStorage:
-    '''FileStorage'''
+    """FileStorage"""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
-        '''all'''
+        """all"""
         return self.__objects
 
     def new(self, obj):
-        '''new'''
+        """new"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        '''save'''
+        """save"""
         dobj = {}
         for key, val in self.__objects.items():
             dobj[key] = val.to_dict()
@@ -29,7 +29,7 @@ class FileStorage:
             dump(dobj, jsonF)
 
     def reload(self):
-        '''reload'''
+        """reload"""
         from models.base_model import BaseModel
 
         definclass = {'BaseModel': BaseModel}
