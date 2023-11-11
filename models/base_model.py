0001deb880840d629402bc85a3c3b9fad7d06a34
HichamLuffy <imalxvssteve2020@gmail.com>
2023-11-12 00:45:58 +0100

    fix comment

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,9 @@
 Usage:
 python3 models/base_model.py
 """
-import uuid
+from uuid import uuid4
 from datetime import datetime
-import models
+from models import storage
 
 
 class BaseModel:
@@ -27,16 +27,18 @@ class BaseModel:
         """
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-            self.created_at = datetime.strptime(self.created_at, time_format)
-            self.updated_at = datetime.strptime(self.updated_at, time_format)
+            del kwargs["__class__"]
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    val = datetime.strptime(val, time_format)
+                    setattr(self, key, val)
+                else:
+                    setattr(self, key, val)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
-        models.storage.new(self)
+            self.updated_at = datetime.now()
+        storage.new(self)
 
     def __str__(self):
         """
@@ -54,7 +56,7 @@ class BaseModel:
         Return: None
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
@@ -64,7 +66,7 @@ class BaseModel:
 
         """
         new_dict = self.__dict__.copy()
-        new_dict["__class__"] = self.__class__.__name__
+        new_dict['__class__'] = self.__class__.__name__
         for key, val in self.__dict__.items():
             if isinstance(val, datetime):
                 new_dict[key] = val.isoformat()
