f10023bb4e9cb9b3ab1a1c9ab914fa1060551936
HichamLuffy <imalxvssteve2020@gmail.com>
2023-11-12 00:42:21 +0100

    fix comment

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
             self.created_at = datetime.strptime(self.created_at, time_format)
             self.updated_at = datetime.strptime(self.updated_at, time_format)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
         storage.new(self)
